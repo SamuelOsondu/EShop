84411782c5800c91928d1fafb9067f70ddc2077f
Osondu Samuel <samuelosondu99@gmail.com>
2023-03-13 18:42:48 +0100

    seventh Commit

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    shipping_address = models.CharField(max_length=250)
+    billing_address = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.user.username
+
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from shop.forms import SignUpForm
+from shop.models import Customer
+
 
 # Create your views here.
 
@@ -28,8 +33,29 @@ def test(request):
 
 
 def login(request):
+
     return render(request, "shop/login.html")
 
 
 def signup(request):
-    return render(request, "shop/signup.html")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'shop/signup.html', {'form': form})
+
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         customer = Customer.objects.create(user=user, shipping_address=request.POST['shipping_address'],
+    #                                            billing_address=request.POST['billing_address'],
+    #                                            phone_number=request.POST['phone_number'])
+    #         return redirect('home')
+    #     else:
+    #         form = UserCreationForm()
+    #
+    #     return render(request, "shop/signup.html", {'form': form})
